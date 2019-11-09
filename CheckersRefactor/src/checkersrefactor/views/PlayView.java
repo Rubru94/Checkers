e0291afbde557312d73c7e3d63df97891990e34e
Rubru94 <Rubru94>
2019-11-09 09:51:32 +0100

    Create Coordinate constructor with String param

--- CheckersRefactor/src/checkersrefactor/views/PlayView.java
+++ CheckersRefactor/src/checkersrefactor/views/PlayView.java
@@ -18,9 +18,10 @@ public class PlayView extends SubView {
         GameView gameView = new GameView();
         do {
             String command = this.console.readString("Mueven las " + color + ": ");
-            int origin = Integer.parseInt(command.substring(0, 2));
-            int target = Integer.parseInt(command.substring(3, 5));
-            error = playController.move(new Coordinate(origin / 10 - 1, origin % 10 - 1), new Coordinate(target / 10 - 1, target % 10 - 1));
+            String[] input = command.split("\\.|\\n");
+            error = playController.move(
+                    new Coordinate(input[0]),
+                    new Coordinate(input[1]));
             if (error != null) {
                 console.writeln("Error!!!" + error.name());
                 gameView.write(playController);
